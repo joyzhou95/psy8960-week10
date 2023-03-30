73bdbfc40aad85eec8b1637d38dde5537f442b82
Joy Zhou <zhou1559@umn.edu>
2023-03-30 16:22:24 -0500

    Visualization created

--- R/week10.R
+++ R/week10.R
@@ -10,3 +10,10 @@ gss_tbl <- read_sav("../data/GSS2016.sav") %>%
              ~na_if(., 99)) %>%
   filter(!is.na(HRS1)) %>%
   select(which(colSums(is.na(gss_tbl))/nrow(gss_tbl) < 0.75))
+
+# Visualization
+gss_tbl %>%
+  ggplot(aes(HRS1)) +
+  geom_bar() +
+  labs(x = "Number of Hourse Worked Last Week", "Frequency")
+
