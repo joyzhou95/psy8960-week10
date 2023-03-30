aa2c00df4470ee002cead53e9a0dff9cd9a8fbd3
Joy Zhou <zhou1559@umn.edu>
2023-03-30 16:19:29 -0500

    Selected variables with less than 75% missingness

--- R/week10.R
+++ R/week10.R
@@ -8,5 +8,5 @@ gss_tbl <- read_sav("../data/GSS2016.sav") %>%
   mutate_all(~na_if(., 0),
              ~na_if(., 98),
              ~na_if(., 99)) %>%
-  filter(is.na(HRS2))
-
+  filter(!is.na(HRS1)) %>%
+  select(which(colSums(is.na(gss_tbl))/nrow(gss_tbl) < 0.75))
